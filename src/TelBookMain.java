2088879f8e0ef5c05d31dd32d3c91256ef0afac8
junghan0811kjh-droid <junghan0811kjh>
2026-04-21 10:40:04 +0900

    feat : 이름, 나이, 전화번호 정규식 처리 및 예외처리 완료

--- src/TelBookMain.java
+++ src/TelBookMain.java
@@ -1,10 +1,11 @@
+import exception.MyException;
 import view.UserView;
 
 import javax.security.sasl.SaslClient;
 import java.util.Scanner;
 
 public class TelBookMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyException {
         Scanner sc = new Scanner(System.in);
 
         // UserView 인스턴스 생성
