e80b2161b4241b0a7718ee2057db7b8b82a6aac6
junghan0811kjh-droid <junghan0811kjh>
2026-04-22 13:01:37 +0900

    feat : Update 처리 완료

--- src/service/TelbookService.java
+++ src/service/TelbookService.java
@@ -34,4 +34,9 @@ public class TelbookService {
     public int delete(int id) {
         return repository.deleteById(id);
     }
+
+    public void update(TelDto updateData) {
+//        System.out.println(updateData);
+        repository.update(updateData);
+    }
 }
