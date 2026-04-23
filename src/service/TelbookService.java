51935210901c9a69fac3dc042a4b3e3badef1d0c
junghan0811kjh-droid <junghan0811kjh>
2026-04-23 14:20:16 +0900

    feat : 검색(이름 또는 주소) 서비스 완료

--- src/service/TelbookService.java
+++ src/service/TelbookService.java
@@ -39,4 +39,9 @@ public class TelbookService {
 //        System.out.println(updateData);
         repository.update(updateData);
     }
+
+    public List<TelDto> search(int choice, String keyword) {
+        return repository.search(choice, keyword);
+    }
+
 }
